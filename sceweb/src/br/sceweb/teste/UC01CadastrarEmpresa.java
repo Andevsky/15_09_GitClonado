c0b5f9c34854022500d84867adcc6b5450549f83
aluno <aluno@L4M16.ipiranga.fatec>
2016-09-15 16:08:49 -0300

    UC01CadastrarEmpresa - Manutenção evolutiva
    Inclusão da empresa na base de dados

---
+++ sceweb/src/br/sceweb/teste/UC01CadastrarEmpresa.java
@@ -0,0 +1,39 @@
+package br.sceweb.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.sceweb.modelo.Empresa;
+import br.sceweb.modelo.EmpresaDAO;
+
+public class UC01CadastrarEmpresa {
+	static Empresa empresa;
+	static EmpresaDAO empresaDAO;
+	
+	/*Estabelece as pre condições de teste*/
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		empresaDAO = new EmpresaDAO();
+		empresa = new Empresa();
+		empresa.setNomeDaEmpresa("empresa x");
+		empresa.setCnpj("89424232000180");
+		empresa.setNomeFantasia("empresa x");
+		empresa.setEndereco("rua taquari");
+		empresa.setTelefone("2222");
+	}
+
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void CT01UC01F8CadastrarEmpresa_com_sucesso() {
+		assertEquals(1,empresaDAO.adiciona(empresa));
+	}
+
+}
